2ebfef0fa60cc725aff06e7acf94fbb9ab84c702
Eya <Eya@EYA-LENOVO-PC>
2024-10-20 10:39:21 +0200

    Add project files.

---
+++ ExerciceRacine/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+
+    public static void CalculeRacine(double number, int root)
+    {
+        if (number >= 1.00 && root >= 1)
+        {
+            double y = 0.00000001;
+            double x = 1;
+            double debut = 1;
+            double Result = 0;
+            while (x < number)
+            {
+                debut = debut + y;
+                x = debut;
+                for (int i = 0; i < root; i++)
+                {
+                    Result = x;
+                    x = x * x;
+                }
+            }
+            Console.WriteLine($"valleur approximative {Result}  ≈  {(int)Result}");
+
+        }
+        else
+        {
+            Console.WriteLine("Erreur Parametre");
+        }
+
+    }
+    private static void Main(string[] args)
+    {
+        CalculeRacine(16, 4);
+        CalculeRacine(9, 3);
+        CalculeRacine(10, 2);
+        CalculeRacine(8, 3);
+
+    }
+}
---
+++ ExercieHashCode/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    public class SameHashcode
+    {
+        public string Valeur;
+
+        public SameHashcode(string valeur)
+        {
+            Valeur = valeur;
+        }
+
+        public override int GetHashCode()
+        {
+            return 50;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            //Good practice lors de l'override de la methode GetHashCode() il faut aussi le faire Equals
+            //Code ...
+            return true;
+
+        }
+    }
+    private static void Main(string[] args)
+    {
+        SameHashcode A = new SameHashcode("Hola");
+        SameHashcode B = new SameHashcode("Salut");
+        SameHashcode C = new SameHashcode("Hello");
+
+        Console.WriteLine(A.Valeur + " HashCode " + A.GetHashCode());
+        Console.WriteLine(B.Valeur + " HashCode " + B.GetHashCode());
+        Console.WriteLine(C.Valeur + " HashCode " + C.GetHashCode());
+
+
+    }
+}
